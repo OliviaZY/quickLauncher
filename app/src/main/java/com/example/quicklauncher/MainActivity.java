2bad326587cfffaa48fd69758c7e781cbc881f57
Yuan zhou <zhouy4@rose-hulman.edu>
2019-05-05 23:31:06 -0500

    launch google that is outside the app

--- app/src/main/java/com/example/quicklauncher/MainActivity.java
+++ app/src/main/java/com/example/quicklauncher/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.quicklauncher;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -21,6 +22,20 @@ public class MainActivity extends AppCompatActivity {
                 // show how to pass info to another activity
                 startIntent.putExtra("com.example.quicklauncher.SOMETHING","hello world");
                 startActivity(startIntent);
+            }
+        });
+
+        Button googleBtn = (Button) findViewById(R.id.googleBtn);
+        googleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String google = "http://www.google.com";
+                Uri webaddress = Uri.parse(google);
+
+                Intent gotoGoogle = new Intent(Intent.ACTION_VIEW, webaddress);
+                if(gotoGoogle.resolveActivity(getPackageManager()) != null) {
+                    startActivity(gotoGoogle);
+                }
             }
         });
     }
